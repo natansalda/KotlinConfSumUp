d54595dac1eb5294a7bbb76f84c597911053fbdb
Natalia Nazaruk <nazarjazz@wp.pl>
2019-12-12 19:30:06 +0100

    Added line between sections

--- app/src/main/java/com/example/kotlinconfsumup/MainActivity.kt
+++ app/src/main/java/com/example/kotlinconfsumup/MainActivity.kt
@@ -25,9 +25,9 @@ class MainActivity : AppCompatActivity() {
             "2. Jetpack Compose may be a revolution." +
                     "It's created in open, so anyone can check and maybe contribute. \n",
             "3. Kotlin 1.4 to arrive in spring 2020" +
-                    ""
+                    "I will speak about this more in the next section \n"
         )
-        list_of_Conclusions.text = listOfConclusions.toString()
+        list_of_conclusions.text = listOfConclusions.toString()
     }
 
     private fun loadChangesInNewVersionOfKotlin() {
@@ -35,24 +35,25 @@ class MainActivity : AppCompatActivity() {
             "1. Focus on quality and performance: " +
                     "* Boosting IDE performance - Faster code completion" +
                     "* Gradle scripts faster - Gradle Import in Android Studio is about 2.5 " +
-                    "times as fast and takes about 75% less memory than it did in Kotlin 1.3.10 /n",
+                    "times as fast and takes about 75% less memory than it did in Kotlin 1.3.10 \n",
             "2. New compiler" +
                     "* Gradual implementation to make it efficient for all platforms" +
                     "* Improvement will be ongoing through the year" +
                     "* both versions will be available for some time, " +
-                    "the old one and the new one in an experimental mode /n",
+                    "the old one and the new one in an experimental mode \n",
             "3. KLib: Kotlin Library Format" +
                     "* library format for Kotlin multiplatform" +
                     "* Kotlin/Native has been using the KLibs format to distribute Kotlin native libraries already, " +
-                    "and now it will be extended to support other back-ends and multiplatform libraries",
+                    "and now it will be extended to support other back-ends and multiplatform libraries \n",
             "4. Other improvements" +
                     "* Running iOS code in Android Studio - " +
                     "plugin for AS that will be able to run, test, and debug Kotlin code on iOS devices and simulators" +
                     "* Introduction to Flow: " +
                     "coroutine-based implementation of Reactive Streams" +
                     "* SAM (single abstract method) conversions for Kotlin classes" +
-                    "* Mixing named and positional arguments"
+                    "* Mixing named and positional arguments \n"
         )
+        list_of_kotlin_improvements.text = listOfKotlinImprovements.toString()
     }
 
     private fun loadTalkGoldenHammer() {
